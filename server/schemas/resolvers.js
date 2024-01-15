aab1088ba425e906c7bea824556d1710e138ac94
Sonja <sonjaljatkovic@gmail.com>
2024-01-14 22:05:14 -0500

    created addUser and login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,10 +9,38 @@ const resolvers = {
 
         me: async (parent, args, context) => {
             if (context.user) {
-                return User.findOne({ _id: context.user._id });
+                return User
+                    .findOne({ _id: context.user._id })
+                    .populate("books")
+                    .select(-__v -password);
             }
             throw AuthenticationError;
-        }
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+    
+            if (!user) {
+                throw AuthenticationError;
+            }
+    
+            const correctPw = await user.isCorrectPassword(password);
+    
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+    
+            const token = signToken(user);
+    
+            return { token, user };
+        },
     }
 }
 
